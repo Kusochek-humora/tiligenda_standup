d40f1817f98deebbbc5b97add2a2abeb791eeda3
Kusochek-humora <69989303+Kusochek-humora@users.noreply.github.com>
2026-04-04 12:11:18 +0500

    Update events.ts

--- src/data/events.ts
+++ src/data/events.ts
@@ -1,17 +1,17 @@
 import type { Event } from "@/types/event";
 
 export const events: Event[] = [
-  {
-    id: 1,
-    title: "Қазақша импровизациялық кеш",
-    // type_event: "Стендап",
-    date: "2026-04-02",
-    time: "21:30",
-    city: [{ id: 1, title: "Алматы" }],
-    address: "ул. Кабанбай Батыра, 71",
-    image: new URL("@/assets/images/f1.png", import.meta.url).href,
-    url: "https://onvibe.me/kk/event/963",
-  },
+  // {
+  //   id: 1,
+  //   title: "Қазақша импровизациялық кеш",
+  //   // type_event: "Стендап",
+  //   date: "2026-04-02",
+  //   time: "21:30",
+  //   city: [{ id: 1, title: "Алматы" }],
+  //   address: "ул. Кабанбай Батыра, 71",
+  //   image: new URL("@/assets/images/f1.png", import.meta.url).href,
+  //   url: "https://onvibe.me/kk/event/963",
+  // },
 
   {
     id: 2,
